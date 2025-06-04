ff53d48bf22fe8cd0d44b8880180f0ea4f6d0d11
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-06-04 12:56:26 +0300

    feat: set up server with Express, CORS, and MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+import authRouter from "./routes/auth.route.js"
+
+import { connectDB } from "./config/db.config.js";
+
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+//Routes
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log("Server running on port http://localhost:"+PORT);
+  connectDB();
 });
+
