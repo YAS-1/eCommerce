db210f7e440e0483b83d6bb6bbbbcf43b549fe05
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-06-09 14:03:27 +0300

    created the user model with its properties created the auth controllers to handle authentication

---
+++ backend/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+    email:{
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: [true, "Please enter your password"],
+        minilength:[6, "Password must be at least 6 characters"]
+    },
+    cartItem:[
+        {
+            quantity:{
+                type: Number,
+                default: 1
+            },
+            product:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            }
+        }
+    ],
+    role:{
+        type: String,
+        enum: ["customer", "admin"],
+        default: "customer"
+    }
+},{
+    timestamps: true});
+
+mongoose.model("User", userSchema);
+
+//Pre save hook to hash passwords before saving to the database
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+//Method to compare passwords
+userSchema.methods.comparePassword = async function (password){
+    return bcrypt.compare(password, this.password);
+};
+
+export default User;
