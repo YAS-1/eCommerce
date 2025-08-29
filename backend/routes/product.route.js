767e64492c6ceaa92509d5de4db63204ff7de2c3
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-08-29 08:06:17 +0300

    Changes in the product controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,18 +1,16 @@
 import express from "express";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts, getAllFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedProduct } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedProduct } from "../controllers/product.controller.js";
 
 const productRoutes = express.Router();
 
 productRoutes.get("/", protectRoute, adminRoute, getAllProducts); //get all products
-productRoutes.get("/featured", getAllFeaturedProducts); //get featured products
+productRoutes.get("/featured", getFeaturedProducts); //get featured products
 productRoutes.post("/", protectRoute, adminRoute, createProduct); //create product
 productRoutes.delete("/:id", protectRoute, adminRoute, deleteProduct); //delete product
 productRoutes.get("/recommendations", getRecommendedProducts); //get recommended products
 productRoutes.get("/category/:category", getProductsByCategory); //get products by category
 productRoutes.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
-
-
 
 
 export default productRoutes;
