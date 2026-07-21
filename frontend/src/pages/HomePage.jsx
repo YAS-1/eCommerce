8d672c26a3dd269b3b2664f21908dc61db1b7a7c
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2026-07-22 00:05:40 +0300

    Updates

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,7 +12,9 @@ const categories = [
 const HomePage = () => {
   return (
     <div>
-      Home page
+      <div className="bg-gray-900">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Categories</h2>
     </div>
   )
 }
